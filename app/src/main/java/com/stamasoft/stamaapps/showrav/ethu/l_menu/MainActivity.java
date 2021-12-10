79677aa507394a817a3fc76f52dd54861058da00
showrav99 <stamaapps@gmail.com>
2021-12-10 11:31:06 +0600

    initial

---
+++ app/src/main/java/com/stamasoft/stamaapps/showrav/ethu/l_menu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stamasoft.stamaapps.showrav.ethu.l_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
